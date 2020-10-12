8a48111b934a153449654de8dc294dec2e6d11b4
Laura Martin <surminus@gmail.com>
2020-10-12 13:27:10 +0100

    Add the job status

--- notify/notify.sh
+++ notify/notify.sh
@@ -3,7 +3,13 @@
 # Notify
 #
 
-BRANCH_NAME="${GITHUB_REF/refs\/head\//}"
+BRANCH_NAME="$(echo "$GITHUB_REF" |cut -d "/" -f3)"
+
+case $JOB_STATUS in
+  success) STATUS="Succeeded";;
+  failure) STATUS="Failed";;
+  cancelled) STATUS="Cancelled";;
+esac
 
 IFS='' read -r -d '' PAYLOAD <<EOF
 {
@@ -13,20 +19,24 @@ IFS='' read -r -d '' PAYLOAD <<EOF
       "type": "section",
       "text": {
         "type": "mrkdwn",
-        "text": "<https://github.com/$GITHUB_REPOSITORY/runs/$GITHUB_RUN_ID|$GITHUB_REPOSITORY (#$GITHUB_RUN_NUMBER)>"
+        "text": "<https://github.com/$GITHUB_REPOSITORY/actions/runs/$GITHUB_RUN_ID|*$GITHUB_REPOSITORY (#$GITHUB_RUN_NUMBER)*>"
       }
     },
     {
       "type": "section",
       "fields": [
         {
-        "type": "mrkdwn",
-        "text": "*Commit*\n$(echo "$COMMIT_MESSAGE" | head -n1)\n(<https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA|${GITHUB_SHA: -6}> / $BRANCH_NAME)"
+          "type": "mrkdwn",
+          "text": "*Status*\n$STATUS"
         },
         {
           "type": "mrkdwn",
           "text": "*Committer*\n$COMMIT_AUTHOR"
-        }
+        },
+        {
+        "type": "mrkdwn",
+        "text": "*Commit*\n$(echo "$COMMIT_MESSAGE" | head -n1)\n(<https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA|${GITHUB_SHA: -6}> / $BRANCH_NAME)"
+        },
       ]
     }
   ]
