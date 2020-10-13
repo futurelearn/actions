8bd4eba6a4056f14d069a7a0c8b71e991ed19eaf
Laura Martin <surminus@gmail.com>
2020-10-13 15:40:18 +0100

    Use temporary file
    We can't use the object key name because it contains slashes, which is
    fine in S3, but not on a filesystem. This uses a temporary file instead.

--- git-s3-backup/run.sh
+++ git-s3-backup/run.sh
@@ -3,6 +3,7 @@ set -eou pipefail
 
 DATE=$(date +%F)
 FILENAME="$GITHUB_REPOSITORY/$DATE.tgz"
+TMPFILE=$(mktemp)
 
-git archive --format=tar HEAD | gzip > "$FILENAME"
-aws s3 cp "$FILENAME" "s3://$S3_BUCKET/$S3_PREFIX/$FILENAME"
+git archive --format=tar HEAD | gzip > "$TMPFILE"
+aws s3 cp "$TMPFILE" "s3://$S3_BUCKET/$S3_PREFIX/$FILENAME"
